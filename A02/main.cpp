09b748cd3ff71a7ecf4a7e3c10636b0188675de9
Niklas Becker <niklasbecker@posteo.de>
2018-04-17 01:06:43 +0200

    mergesort

--- A02/main.cpp
+++ A02/main.cpp
@@ -11,6 +11,8 @@ void Bubblesort(uint* feld, size_t n);
 void SelectionSort(uint* feld, size_t n);
 void InsertionSort(uint* feld, size_t n);
 void Quicksort(uint* feld, size_t n);
+void QuicksortMedian3(uint* feld, size_t n);
+void MergeSort(uint* feld, size_t n);
 
 int main(int argc, char **argv)
 {
@@ -22,8 +24,10 @@ int main(int argc, char **argv)
 	//Test(&SelectionSort, laenge)
 	//std::cout << "InsertionSort" << std::endl;
 	//Test(&InsertionSort, laenge);
-	std::cout << "QuickSort" << std::endl;
-	Test(&Quicksort, laenge);
+	//std::cout << "QuickSort" << std::endl;
+	//Test(&Quicksort, laenge);
+	std::cout << "MergeSort" << std::endl;
+	Test(&MergeSort, laenge);
 	
 	
 	return 0;
@@ -77,11 +81,9 @@ void InsertionSort(uint* feld, size_t n)
 	}
 }
 
-void Quicksort(uint* feld, size_t n)
+size_t DivideAndConquer(uint* feld, size_t n, size_t pivot) 
 {
-	if(n <= 1) return;
-	size_t pivot, i, j;
-	pivot = n-1;
+	size_t i, j;
 	i = 0; j = n-2;
 	while(i < j) {
 		while(feld[i] < feld[pivot] && i < pivot)
@@ -93,7 +95,58 @@ void Quicksort(uint* feld, size_t n)
 	}
 	if(feld[i] > feld[pivot])
 		tausche(feld, i, pivot);
+	return i;
+}
+
+void Quicksort(uint* feld, size_t n)
+{
+	if(n <= 1) return;
+	size_t i = DivideAndConquer(feld, n, n-1);
 	Quicksort(feld, i);
 	Quicksort(feld + i+1, n-i-1);
+}
+
+void QuicksortMedian3(uint* feld, size_t n)
+{
+	if(n <= 1) return;
+	size_t pivot = n-1;
+	if(n > 2) {
+		if((feld[1] <= feld[n/2] && feld[n/2] <= feld[n-1]) || (feld[n-1] <= feld[n/2] && feld[n/2] <= feld[1]))
+			pivot = n/2;
+		if((feld[n/2] <= feld[1] && feld[1] <= feld[n-1]) || (feld[n-1] <= feld[1] && feld[1] <= feld[n/2]))
+			pivot = 1;
+	}	
+	size_t i = DivideAndConquer(feld, n, pivot);	// adjust pivot algorithm here
+	QuicksortMedian3(feld, i);
+	QuicksortMedian3(feld + i+1, n-i-1);
+}
+
+void MergeSort(uint* feld, size_t n)
+{
+	if(n <= 1) return;
+	size_t p = n/2;
+	uint* f1 = new uint[p];
+	uint* f2 = new uint[n-p];
+	for(size_t i = 0; i < p; i++)
+		f1[i] = feld[i];
+	for(size_t i = 0; i < n-p; i++)
+		f2[i] = feld[p+i];
+	MergeSort(f1, p);
+	MergeSort(f2, n-p);
+	// Merge
+	size_t i1 = 0, i2 = 0;
+	for(size_t i = 0; i < n; i++) {
+		if((f1[i1] <= f2[i2] && i1 < p) || i2 >= n-p ) {
+			feld[i] = f1[i1];
+			i1++;
+		} else  {
+			feld[i] = f2[i2];
+			i2++;
+		}
+	}
+	//for(size_t i = 0; i < n; i++)
+	//	std::cout << feld[i] << (i < n-1 ? '\t' : '\n');
+	
+	delete [] f1; delete [] f2;
 }
 
